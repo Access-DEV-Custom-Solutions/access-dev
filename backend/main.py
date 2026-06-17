1686f931741ae7eb969f457183a8d31d0c38c930
Malvinhaparimwi <malvinhaparimwi@gmail.com>
2026-06-17 23:05:42 +0200

    can insert into request table

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,19 @@ class Users(SQLModel, table=True):
     lastname: str = Field(index=True)
     email: str = Field(index=True, primary_key=True)
 
+class Requests(SQLModel, table=True):
+    email: str = Field(index=True, primary_key=True)
+    service: str = Field(index=True)
+    details: str = Field(index=True)
+
+
+class UserRequest(SQLModel):
+    firstname: str 
+    lastname: str 
+    email: str 
+    service: str
+    details: str
+
 load_dotenv()
 CONNECTION_STRING = os.environ.get("DB_URL")
 
@@ -27,3 +40,16 @@ def add_user(user: Users):
         session.commit()
         session.refresh(user)
         return user
+    
+@app.post("/requests")
+def add_request(userRequest: UserRequest):
+    user = Users(firstname=userRequest.firstname, lastname=userRequest.lastname, email=userRequest.email)
+    request = Requests(email=userRequest.email, service=userRequest.service, details=userRequest.details)
+
+    with Session(engine) as session:
+        session.add(user)
+        session.add(request)
+        session.commit()
+        session.refresh(user)
+        session.refresh(request)
+        return {"message": "done succesffully"}
